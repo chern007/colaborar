79ecfdcbd95f4f00626b46a56d3686f81da88e59
chern007 <carlos.hernandez_crespo@nokia>
2017-10-26 10:33:30 +0200

    para entregar

--- colaborar/src/colaborar/colaborar.java
+++ colaborar/src/colaborar/colaborar.java
@@ -10,27 +10,31 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
+ * Aplicación que ejecuta 10 veces la aplicación lenguaje y guarda el resultado
+ * en el fichero de salida que especifiquemos.
  *
  * @author chern007
  */
+
 public class colaborar {
 
     /**
-     * @param args the command line arguments
+     * @param args (ruta del archivo lenguaje.jar , ruta del archivo de salida).
      */
-    public static void main(String[] args) {  
-
+    
+    public static void main(String[] args) {
 
         try {
-
+            //guardamos los argumentos que le pasamos
             String rutaJar = args[0] + " ";
             String rutaOutput = " " + args[1];
-            
+
+            //declaramos la variable de la clase proceso
             Process p;
 
             for (int i = 1; i < 11; i++) {
 
-                p = Runtime.getRuntime().exec("cmd /C java -jar " + rutaJar + i*10 + rutaOutput);
+                p = Runtime.getRuntime().exec("cmd /C java -jar " + rutaJar + i * 10 + rutaOutput);
 
                 // se obtiene el stream de salida del programa 
                 InputStream is = p.getInputStream();
@@ -38,7 +42,7 @@ public class colaborar {
                 //se prepara un bufferedReader para poder leer la salida más comodamente.
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-                // Se lee la primera linea 
+                //leemos la primera linea 
                 String aux = br.readLine();
 
                 // mientras se haya leido alguna linea 
@@ -46,7 +50,7 @@ public class colaborar {
                     // se escribe la linea en pantalla 
                     System.out.println(aux);
 
-                    // y se lee la siguiente. 
+                    // y se lee la siguiente
                     aux = br.readLine();
                 }
             }
@@ -55,6 +59,7 @@ public class colaborar {
 
         } catch (Exception e) {
             System.err.println("Se ha producido un error al ejecutar el programa externo.");
+            System.err.println(e.toString());
         }
 
     }
